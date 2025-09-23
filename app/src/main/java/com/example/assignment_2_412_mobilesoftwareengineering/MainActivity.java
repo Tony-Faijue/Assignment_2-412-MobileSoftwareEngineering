beda37fe856332efc907a0e373e49334d3a36f99
Tony Faijue <tony.faijue@ndus.edu>
2025-09-23 15:19:21 -0500

    Added Second Activity & Layout and Buttons

--- app/src/main/java/com/example/assignment_2_412_mobilesoftwareengineering/MainActivity.java
+++ app/src/main/java/com/example/assignment_2_412_mobilesoftwareengineering/MainActivity.java
@@ -5,16 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 /**
  * Main Activity of the Android application
+ * Use of an android jetpack class to use newer UI features
  */
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
+        //inflate the layout
         setContentView(R.layout.activity_main);
-        System.out.println(R.id.fullName);
-        String appName = getString(R.string.app_name);
-        String firstName = getString(R.string.first_name);
-        String lastName = getString(R.string.last_name);
-        String fullName = firstName + " " + lastname;
+        //initialize UI components
+
+        //setup Listeners
+
+        //System.out.println(R.id.fullName);
+        //String appName = getString(R.string.app_name);
+        //String firstName = getString(R.string.first_name);
+        //String lastName = getString(R.string.last_name);
+        //String fullName = firstName + " " + lastname;
     }
 }
---
+++ app/src/main/java/com/example/assignment_2_412_mobilesoftwareengineering/SecondActivity.java
@@ -0,0 +1,11 @@
+package com.example.assignment_2_412_mobilesoftwareengineering;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+    }
+}
